3e806d5a9ce3e6f6ac139da2d621ddb57a60052c
Carter.Weinberg <weinbergcarter@gmail.com>
2017-07-02 18:49:55 -0400

    I am now working on accessing the data inside of my grid

---
+++ Assets/Scripts/GridBehaviorTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamelogic.Grids2;
+
+
+public class GridBehaviorTest : GridBehaviour <GridPoint2, TileCell> {
+	
+	public override void InitGrid () { // when the grid is initialized, do things. 
+
+	}
+
+}
